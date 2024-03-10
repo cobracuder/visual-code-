c2216210f6a807d252e25b42c69b93b7e483143f
anirudh <lookingforanirudh@gmail.com>
2024-03-10 22:12:41 +0530

    stride added

---
+++ stride.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+void generate_data(int* val,int numele){
+    srand(time(0));
+    for(int i = 0 ;i<numele;i++) {
+        val[i]=rand()%1000;
+    }
+}
+void display(int* val, int*stride,int *shape,int rank) {
+for(int i=1;i<rank;i++) {
+    shape[i]=stride[i-1]/stride[i];
+} 
+for(int i = 0;i<shape[0];i++){
+    for(int j=0;j<shape[1];j++){
+        cout<<val[i*stride[0]+j*stride[1]]<<" ";
+    }
+    cout<<endl;
+} 
+}
+void display2(int *val, int *stride, int *shape, int rank, int totalSize) {
+    for (int i = 0; i < totalSize; i++) {
+        int index = 0;
+        int remainder = i;
+        // Calculate index in the non-contiguous tensor
+        for (int j = rank - 1; j >= 0; j--) {
+            index += (remainder % shape[j]) * stride[j];
+            remainder /= shape[j];
+        }
+        // Copy value from non-contiguous tensor to contiguous tensor
+        cout<< val[index]<<" ";
+    }
+
+    // Return the contiguous tensor
+    
+}
+
+
+int main() {
+    int shape[]={4,4,5};
+    int rank=sizeof(shape)/sizeof(int);
+    int stride[]={20,5,1};
+    int nonconti[]={4,1,4};
+    int numele=1;
+    for(int i=0;i<rank;i++){
+        numele*=shape[i];
+    }
+    int*val=(int*)(malloc(numele*sizeof(int)));
+    generate_data(val,numele);
+   // display(val,non,shape,rank);
+    cout<<"\ncontigous case:\n";
+    display2(val,stride,shape,rank,numele);
+    cout<<"\nnoncontiguous case:\n";
+    display2(val,nonconti,shape,rank,numele);
+}
--- test.cpp
+++ test.cpp
@@ -1,33 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-using v = std::vector<int>;
-
-#define l(k) ((k?k:new v())->size())
-
-int foo(v* s=nullptr)
-{
-    static v* x= NULL;
-    v* y =NULL;
-    static int r;
-    s?x=s,y=s:s;
-    r+=l(x);
-    r-=l(y);
-    return r;
-}
 int main()
 {
-   
-  vector<int> vec1;
-  vec1.assign({1,2,3,4});
-  v vec2(4,0);
+    int *a = new int(10*sizeof(int));
+    for(int i=0;i<10;i++)
+    a[i]=i;
 
-  foo(&vec1);foo();
-  foo(&vec2);foo();
-
-  cout<<"ams:"<<foo();
-  return 0;
-
-
+    cout<< (*(a-1+2));
 
 }
